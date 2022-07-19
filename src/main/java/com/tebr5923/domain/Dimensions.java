c4b2044e7298679f3c8e6d1e72d323aad726dcb8
Dmitry Tebryaev <tebr5923@mail.ru>
2022-07-19 14:13:49 +0700

    add Machine

---
+++ src/main/java/com/tebr5923/domain/Dimensions.java
@@ -0,0 +1,11 @@
+package com.tebr5923.domain;
+
+public class Dimensions {
+
+    private int x;
+
+    private int y;
+
+    private int z;
+
+}
---
+++ src/main/java/com/tebr5923/domain/model/Machine.java
@@ -0,0 +1,12 @@
+package com.tebr5923.domain.model;
+
+import com.tebr5923.domain.Dimensions;
+
+public class Machine {
+
+    private long id;
+
+    private String name;
+
+    private Dimensions dimensions;
+}
