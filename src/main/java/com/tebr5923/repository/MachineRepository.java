709675a68856e1755c4f53f0d448c008a0869ba6
Dmitry Tebryaev <tebr5923@mail.ru>
2022-07-19 23:40:57 +0700

    add MachineRepository

---
+++ src/main/java/com/tebr5923/repository/MachineRepository.java
@@ -0,0 +1,8 @@
+package com.tebr5923.repository;
+
+import com.tebr5923.domain.model.Machine;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MachineRepository extends CrudRepository<Machine, Long> {
+
+}
