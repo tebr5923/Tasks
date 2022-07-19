f43e4affdf5080ac1c4b23db86568864afeebca7
Dmitry Tebryaev <tebr5923@mail.ru>
2022-07-20 02:10:27 +0700

    add @EnableTransactionManagement

--- src/main/java/com/tebr5923/config/SpringPersistenceConfig.java
+++ src/main/java/com/tebr5923/config/SpringPersistenceConfig.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.tebr5923")
 @PropertySource("classpath:db.properties")
+@EnableTransactionManagement
 public class SpringPersistenceConfig {
     @Value("${db.driver}")
     private String driver;
